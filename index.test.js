6069935c025c6058431d6a3bfc0ab5e442444cd9
Sirius Black <mariia.beliaev@gmail.com>
2023-04-19 23:43:51 +0300

    added tests

--- index.test.js
+++ index.test.js
@@ -1,1 +1,15 @@
 const isAnagrams = require("./index");
+
+describe("Anagrams", () => {
+  test("Function should return true if strings are anagrams", () => {
+    expect(isAnagrams("finder", "FRienD")).toBe(true);
+    expect(isAnagrams("hello", "world")).toBe(false);
+    expect(isAnagrams("melon", "lemon")).toBeTruthy;
+    expect(isAnagrams("abcde2", "c2abed")).toBe(true);
+    expect(isAnagrams("kilso", "osilk")).toBe(true);
+  });
+
+  test("Function should be defined", () => {
+    expect(isAnagrams).toBeDefined();
+  });
+});
