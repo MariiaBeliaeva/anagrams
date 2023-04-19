86fe13f52a104f9573d761be2d2829eb4961c853
Sirius Black <mariia.beliaev@gmail.com>
2023-04-19 18:48:50 +0300

    added anagrams function

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 //Напиши метод isAnagrams(), который принимает два строчных аргумента
 // и выводит true, если строки являются анаграммами, и false, если строки ими не являются.
 
-function isAnagrams(str1, str2){
+function isAnagrams(str1, str2) {
+  let regExp = /[^\w]/g;
 
+  const sortedStr1 = str1
+    .toLowerCase()
+    .replace(regExp, "")
+    .split("")
+    .sort()
+    .join("");
+  const sortedStr2 = str2
+    .toLowerCase()
+    .replace(regExp, "")
+    .split("")
+    .sort()
+    .join("");
+
+  return sortedStr1 === sortedStr2;
 }
+console.log(isAnagrams("finder", "FRienD"));
+console.log(isAnagrams("hello", "world"));
+
+module.exports = isAnagrams;
---
+++ index.test.js
@@ -0,0 +1,1 @@
+const isAnagrams = require("./index");
